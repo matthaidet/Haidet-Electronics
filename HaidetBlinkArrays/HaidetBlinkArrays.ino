31159e07beac656cf94f823b255cb2d55236ab8a
Matt Haidet <plusallofyou@gmail.com>
2014-12-15 12:05:42 -0500

    added array assignment

--- HaidetBlinkArrays/HaidetBlinkArrays.ino
+++ HaidetBlinkArrays/HaidetBlinkArrays.ino
@@ -1,27 +1,31 @@
-//Haidet LED Blink Arrays
-
-void pattern(){
+//Classwork: LED Array Assessment
+//Matt Haidet
+//All work here is honestly obtained and is my own
   byte pins[] = {2,3,4,5,6,7,8,9};
-  for(byte i = 1; i < 7; i++){
-    blink(pins[i]);
+  
+  void pattern(){
+
+  for(byte i = 0; i <= 7; i++){
+    flash(pins[i]);
   }
     for(byte i = 7; i >=1 ; i--){
-    blink(pins[i]);
+    flash(pins[i]);
   }
 }
 
-void blink(byte i){
+void flash(byte i){
     digitalWrite(i, HIGH);
     delay(500);
     digitalWrite(i,LOW);
 }
 
+
 void setup(){
-  for(byte i = 2; i <10; i++){
+  for(byte i = 0; i <10; i++){
    pinMode(i, OUTPUT);
   }
 }
 
 void loop(){
-  pattern();
+pattern();
 }
